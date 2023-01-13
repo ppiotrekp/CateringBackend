861a6db611f169e2c43e5625b8fb683c0f0b4fbc
ppyrc <ppyrczak@student.agh.edu.pl>
2023-01-13 19:54:17 +0100

    registration request class created

---
+++ src/main/java/pl/ppyrczak/cateringbackend/registration/RegistrationRequest.java
@@ -0,0 +1,21 @@
+package pl.ppyrczak.cateringbackend.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String username;
+    private final String password;
+    private final Set<? extends GrantedAuthority> authorities;
+}
