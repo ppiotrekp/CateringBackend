2bd528c62828c9fb3035de7620261679c44dce3c
ppyrc <ppyrczak@student.agh.edu.pl>
2023-01-13 19:52:35 +0100

    implementing UserDetails

--- src/main/java/pl/ppyrczak/cateringbackend/model/User.java
+++ src/main/java/pl/ppyrczak/cateringbackend/model/User.java
@@ -1,4 +1,55 @@
 package pl.ppyrczak.cateringbackend.model;
 
-public class User {
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Data
+@Document
+public class User implements UserDetails {
+    @Id
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
