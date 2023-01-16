daf960df648cb0e614dcb29bd608c2c0392abab6
ppyrc <ppyrczak@student.agh.edu.pl>
2023-01-16 12:31:13 +0100

    auth request and response to handle jwt

---
+++ src/main/java/pl/ppyrczak/cateringbackend/login/AuthResponse.java
@@ -0,0 +1,11 @@
+package pl.ppyrczak.cateringbackend.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+}
