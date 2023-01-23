fa1e8b5931a38d3311b5fd3484e558679fe1c4fc
ppyrc <ppyrczak@student.agh.edu.pl>
2023-01-23 12:21:34 +0100

    update dish created

--- src/main/java/pl/ppyrczak/cateringbackend/controller/DishController.java
+++ src/main/java/pl/ppyrczak/cateringbackend/controller/DishController.java
@@ -1,6 +1,7 @@
 package pl.ppyrczak.cateringbackend.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.ppyrczak.cateringbackend.model.Dish;
 import pl.ppyrczak.cateringbackend.service.DishService;
@@ -29,8 +30,14 @@ public class DishController {
     }
 
     @ResponseStatus(OK)
-    @GetMapping("/dishes/page")
+    @GetMapping("/manager/dishes")
     public List<Dish> getAllDishes(@RequestParam(required = false) int page) {
+        return dishService.getAllDishes(page);
+    }
+
+    @ResponseStatus(OK)
+    @GetMapping("/guest/dishes")
+    public List<Dish> getDishesForGuests(@RequestParam(required = false) int page) {
         return dishService.getAllDishes(page);
     }
 
@@ -44,5 +51,11 @@ public class DishController {
     @DeleteMapping("/dish/{id}")
     public void removeDish(@PathVariable String id) {
         dishService.removeDish(id);
+    }
+
+    @ResponseStatus(OK)
+    @PutMapping("/dish/{id}")
+    public Dish updateDish(@RequestBody Dish dish,@PathVariable String id) {
+        return dishService.updateDish(dish, id);
     }
 }
